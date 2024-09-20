e40c0e02c78c281d5e393838a975376268280de9
dainK <dain940209@gmail.com>
2024-09-20 22:56:29 +0900

    [level 0] Title: 글자 지우기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -0,0 +1,8 @@
+function solution(my_string, indices) {
+    var answer = '';
+    for(var i = 0; i<my_string.length;i++) {
+        if(!indices.includes(i))
+            answer+=my_string[i];
+    }
+    return answer;
+}
