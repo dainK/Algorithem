8fcc0fcf94367713f89acbcedd07687159143344
dainK <dain940209@gmail.com>
2024-09-20 20:58:46 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    var answer = 0;
+    var odd=0;
+    var even=0;
+    for(var i = 0; i<num_list.length;i++) {
+        if(i%2==0) {
+            even+=num_list[i];
+        }
+        else {
+            odd+=num_list[i];
+        }
+    }
+    return even>odd?even:odd;
+}
