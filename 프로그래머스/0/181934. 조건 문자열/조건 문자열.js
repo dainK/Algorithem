cacc5ad8e40ec1e6e4d882da3a3bd34022ee00fa
dainK <dain940209@gmail.com>
2024-10-07 21:40:46 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+    if(ineq == '>') {
+        if(eq == '=') {
+            return +(n >= m);
+        }
+        else {
+            return +(n > m);
+        }
+    }
+    else {
+        if(eq == '=') {
+            return +(n <= m);
+        }
+        else {
+            return +(n < m);
+        }
+    }
+}
